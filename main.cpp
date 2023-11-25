0d970a8738f7021dc5988423673b63e6727b6f7b
BuildTools <unconfigured@null.spigotmc.org>
2023-11-25 21:58:31 +0000

    Added release.
    Added vertex shader

--- main.cpp
+++ main.cpp
@@ -4,17 +4,28 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <string>
+#include <filesystem>
+#include <limits>
+#include <fstream>
+#include <iterator>
+
 using namespace std;
 using namespace glm;
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
+const char *vertexShaderSource = "#version 460 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "}\0";
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
     glViewport(0, 0, width, height);
 }
 
-void processInput(GLFWwindow *window)
-{
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
 
@@ -59,9 +70,41 @@ int main() {
     // Set the background colour for the window.
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
+    // Simple NDC data to render a triangle
+    float vertices[] = {
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f, 0.5f, 0.0f
+    };
 
-    while(!glfwWindowShouldClose(window))
-    {
+    // Create a vertex buffer object to store our vertices on the GPU.
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+
+    // Set the VBO as the active Array Buffer object.
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    // Copy the vertices data into the buffer.
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Create a shader and store an ID number for it.
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+    // Attach the shader source code and compile.
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        cerr << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << '\n';
+    }
+
+    while (!glfwWindowShouldClose(window)) {
         // Input Processing
         processInput(window);
 
