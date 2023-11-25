86aa9d5a7e50af3c2352c3e914fe2447a278f39c
BuildTools <unconfigured@null.spigotmc.org>
2023-11-25 20:57:36 +0000

    Got a black window showing

--- main.cpp
+++ main.cpp
@@ -7,11 +7,49 @@
 using namespace std;
 using namespace glm;
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
 int main() {
-    glfwInit();
+    if (!glfwInit()) {
+        cout << "GLFW Failed to initiate\n";
+        return 1;
+    }
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    const unsigned int windowWidth = 1280;
+    const unsigned int windowHeight = 720;
+
+    GLFWwindow *window = glfwCreateWindow(windowWidth, windowHeight, "OpenGL Ray Tracing", NULL, NULL);
+
+    if (window == NULL) {
+        cout << "Failed to create GLFW Window\n";
+        glfwTerminate();
+        return 2;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        cout << "Failed to initialise GLAD\n";
+        return 3;
+    }
+
+    glViewport(0, 0, windowWidth, windowHeight);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
